ea25d1ac667d71989ccc08537c9810960e24a815
ka53y <Kasey.a.schoeff@gmail.com>
2021-09-10 12:20:48 -0400

    first Half

--- array-source/Array.cpp
+++ array-source/Array.cpp
@@ -6,6 +6,7 @@
 // on this assignment.
 
 #include "Array.h"
+#include "iostream"
 
 Array::Array (void)//:data_(), cur_size_(0), max_size_(0)
 { 
@@ -34,7 +35,7 @@ Array::Array (size_t length, char fill)//: cur_size_(length), max_size_(length)
 
 Array::Array (const Array & array)
 {
-
+ 
 }
 
 Array::~Array (void)
@@ -59,6 +60,15 @@ const char & Array::operator [] (size_t index) const
 
 char Array::get (size_t index) const
 {
+    try
+    {
+        data_[index] = "x";
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
     return data_[index];
     //todo: throw an exception if its out of range below 0 or above max value
 
@@ -67,56 +77,135 @@ char Array::get (size_t index) const
 void Array::set (size_t index, char value)
 {
     //setting the char value at the location index in the array data_
+    try
+    {
+        data_[index] = 10;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     data_[index] = value;
     // TODO: throw an exception if it is out of range
 }
 
 void Array::resize (size_t new_size)
 {
+    char newArr[new_size];
+    for (size_t i = 0; i < new_size; i++)
+    {
+        newArr[i] = data_[i];
+    }
+
+    
     //destroy the array save values and add the values to a new array of size new_size
 }
 
 int Array::find (char ch) const
 {
-
+    for (size_t i = 0; i < cur_size_; i++)
+    {
+        if(data_[i] == ch){
+            return i;
+        }
+    }
+    return -1;
+    
 }
 
 int Array::find (char ch, size_t start) const
 {
-
+    for (size_t i = start; i < cur_size_; i++)
+    {
+        if(data_[i] == ch){
+            return i;
+        }
+    }
+    return -1;
 }
 
 bool Array::operator == (const Array & rhs) const
 {
-
+    for (size_t i = 0; i < cur_size_; i++)
+    {
+        if(data_[i] != rhs[i]){
+            return false;
+        }
+    }
+    return true;
+    
 }
 
 bool Array::operator != (const Array & rhs) const
 {
+    size_t i = 0;
 
+    do{
+        i++;
+    } while(rhs[i] == data_[i]);
+
+    if(i == max_size_){
+        return false;
+    } else{
+        return true;
+    }
+    
 }
 
 void Array::fill (char ch)
 {
-
+    for (size_t i = 0; i < max_size_; i++)
+    {
+        data_[i] = ch;
+    }
+    
 }
 
 void Array::shrink (void)
 {
-  
+  size_t newSize;
+  for (size_t i = 0; i < cur_size_; i++)
+  {
+      if(data_[i] == NULL){
+          newSize++;
+      }
+  }
+  newSize = cur_size_ - newSize;
 }
 
 void Array::reverse (void)
 {
+  char tempdata[cur_size_];
+  for (size_t i = cur_size_; i > 0; i--)
+  {
+      tempdata[i] = data_[i];
+  }
+  for (size_t i = 0; i < cur_size_; i++)
+  {
+      data_[i] = tempdata[i];
+  }
   
+   
 }
 
 Array Array::slice (size_t begin) const
 {
+  char slicedArr[cur_size_];
+  char *pArr = slicedArr;
+  for (size_t i = begin; i < cur_size_; i++)
+  {
+      slicedArr[i] = data_[i];
+  }
+  return pArr;
   
 }
 
 Array Array::slice (size_t begin, size_t end) const
 {
-  
+  char * slicedArr = new char[cur_size_];
+  for (size_t i = begin; i < end; i++)
+  {
+      slicedArr[i] = data_[i];
+  }
+  return slicedArr;
 }
